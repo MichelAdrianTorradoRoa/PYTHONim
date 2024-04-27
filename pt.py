31e14f152aefbdb5b0bc763c9130bed287910e61
Michel Adrian Torrado Roa <77382674+MichelAdrianTorradoRoa@users.noreply.github.com>
2024-04-27 18:53:40 -0500

    Feat.py

---
+++ pt.py
@@ -0,0 +1,15 @@
+print("Ingrese su nombre")
+name= str(input(""))
+print("Ingrese su sexo biologico (F o M)")
+gender=str(input(""))
+
+if gender=="F":
+    if name.lower()<"m":
+        input("Su grupo es A")
+    else:
+        input("Su grupo es B")
+else:
+    if name.lower()<"n":
+        input("Su grupo es A")
+    else:
+        input("Su grupo es B")    
